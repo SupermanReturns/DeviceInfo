6e17c45a1b77a3b26c7a4fbb36822dfc07656cdb
Superman <703353105@qq.com>
2018-07-20 16:32:24 +0800

    Initial Commit

---
+++ DeviceInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeviceInfo
+//
+//  Created by Superman on 2018/7/20.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
